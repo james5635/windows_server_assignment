fed768375b82bbbb587bcadd92d29b13f82f1031
james5635 <brorojame2029@gmail.com>
2025-12-20 16:12:09 +0700

    add nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Codespaces Nix env";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs
+        go
+        python312
+      ];
+    };
+  };
+}
